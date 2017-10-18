ce69d22dd5515530f42727abf2b9f496cad7e468
DanielATV <daniel.torov@sansano.usm.cl>
2017-10-18 19:33:10 -0400

    Update dispersa.h

--- dispersa.h
+++ dispersa.h
@@ -1,16 +1,16 @@
 #ifndef _DISPERSA_H_
 #define _DISPERSA_H_
 
-	void* alloc_dispersa(int m, int n);
-	void free_dispersa(void* A);
-	int ingresar_valor(void* A, int i, int j, int valor);
-	void imprimir_matriz(void* A);
-	void* suma(void*A,void*B);
-	void* multiplicacion(void*A,void*B);
-	void binaria(void* (*fun)(void*,void*), void* A, void* B);
-	void* transponer(void* A);
-	void* diagonal(void* A);
-	void unaria(void* (*fun)(void*), void* A);
-	void* element_wise_op(int (*fun)(int), void* A);
+void* alloc_dispersa(int m, int n);
+void free_dispersa(void* A);
+int ingresar_valor(void* A, int i, int j, int valor);
+void imprimir_matriz(void* A);
+void* suma(void*A,void*B);
+void* multiplicacion(void*A,void*B);
+void binaria(void* (*fun)(void*,void*), void* A, void* B);
+void* transponer(void* A);
+void* diagonal(void* A);
+void unaria(void* (*fun)(void*), void* A);
+void* element_wise_op(int (*fun)(int), void* A);
 
 #endif
