176965a52f2351053ff630759493aeeb2d6caad1
DanielATV <daniel.torov@sansano.usm.cl>
2017-10-20 01:11:27 -0300

    Update matris.h

--- matris.h
+++ matris.h
@@ -1,16 +1,18 @@
 #ifndef __matris_h__
 #define __matris_h__
 
-
-typedef struct elemento{
- int comienzo;
- int final;
-} elemento;
-
 typedef struct nodo {
- elemento info; // el elemento en sí
- struct nodo *sig; // puntero autoreferencial
-                   // al proximo nodo
+	int i;
+	int j;
+	int valor;
+	struct nodo *sig; 
+                   
 } tNodo;
+
+typedef struct matris{
+	int filas;
+ 	int columnas;
+	tNodo *arreglo;
+} tMatris;
 
 #endif
