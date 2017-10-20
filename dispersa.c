f1a042c07de3f00296f7aaab332191e1f3a44e95
DanielATV <daniel.torov@sansano.usm.cl>
2017-10-20 01:11:59 -0300

    Update dispersa.c

--- dispersa.c
+++ dispersa.c
@@ -1,1 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "matris.h"
+
+
+
+void* alloc_dispersa(int m, int n){
+	tMatris *pter;
+	tNodo *pt;
+	int i;
+	tNodo aux;
+	aux.sig = NULL;
+
+	pter = (tMatris *)malloc(sizeof(tMatris));
+	pt = (tNodo *)malloc(n*sizeof(tNodo));
+	pter->filas = m;
+	pter->filas = n;
+	pter->arreglo = pt;
+
+	for (i = 0; i < n; i++){
+		(pter->arreglo)[i] = aux;
+	}
+
+
+	return (void *)pter;
+}
+void free_dispersa(void* A){
+
+}
