01113dca87cf32e1405f4b0846408160ea7192b2
DanielATV <daniel.torov@sansano.usm.cl>
2017-10-22 14:58:30 -0300

    Update main.c

--- main.c
+++ main.c
@@ -5,27 +5,44 @@
 
 int main(){
 	void *a;
-	int b,c;
+	void *b;
+	void *c;
+	
 
 	a = alloc_dispersa(2, 2);
+	b = alloc_dispersa(2, 2);
 
 
-	ingresar_valor(a,1,1,3);
-	ingresar_valor(a,0,1,4);
-	ingresar_valor(a,0,0,2);
-	ingresar_valor(a,1,0,7);
+	ingresar_valor(a,1,1,1);
+	ingresar_valor(a,0,1,1);
+	ingresar_valor(a,0,0,1);
+	ingresar_valor(a,1,0,1);
 
 	imprimir_matriz(a);
 
 	printf("----------------\n");
 
-	ingresar_valor(a,1,0,9);
+	ingresar_valor(b,1,1,2);
+	ingresar_valor(b,0,1,0);
+	ingresar_valor(b,0,0,0);
+	ingresar_valor(b,1,0,2);
 
 
-	imprimir_matriz(a);
+	imprimir_matriz(b);
+
+	printf("----------------\n");
+
+
+	c = multiplicacion(a,b);
+
+	imprimir_matriz(c);
+
+
 
 
 	free_dispersa(a);
+	free_dispersa(b);
+	free_dispersa(c);
 
 
 
