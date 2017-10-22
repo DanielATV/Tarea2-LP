1c7790aedc8305d306c3790853da360c93736ddc
DanielATV <daniel.torov@sansano.usm.cl>
2017-10-22 14:59:32 -0300

    Update matris.c

--- matris.c
+++ matris.c
@@ -1,1 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "matris.h"
+
+int encontrar_valor(void* A, int i, int j){
+	tNodo *pt;
+	tNodo *ptaux;
+	tNodo aux;
+	int valor;
+	valor = 0;
+
+	aux = (((tMatris *)A)->arreglo)[j];
+	pt = aux.sig;
+
+	if (pt == NULL) return valor;
+
+	else{
+
+		while (pt != NULL){
+
+			if(pt->i == i){
+
+				valor = pt->valor;
+				return valor;
+
+			}
+
+
+
+			ptaux = pt -> sig;
+			pt = ptaux;
+		}
+		return valor;
+	}
+	
+}
